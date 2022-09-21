8238200d7ff74a9e1b10a40a786016b1747dc344
amber harris <amberlharris22@gmail.com>
2022-09-21 14:41:29 -0600

    second try

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/',express.static(path.join(__dirname, '../deployment/client/index.html')))
-app.use(express.static(path.join(__dirname, '../deployment/client')))
+// app.use('/',express.static(path.join(__dirname, '../deployment/client/index.html')))
+// app.use(express.static(path.join(__dirname, '../deployment/client')))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../deployment/client/index.html'))
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../deployment/client/index.html'))
+})
 
 app.listen(port, () => {
     console.log('Docked at port ' + port)

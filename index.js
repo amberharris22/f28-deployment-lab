ff5c55b930b563c5b4ccbf513a732e2343b7480e
amber harris <amberlharris22@gmail.com>
2022-09-21 14:34:13 -0600

    boiler plate

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+
+app.use('/',express.static(path.join(__dirname, '../deployment/client/index.html')))
+app.use(express.static(path.join(__dirname, '../deployment/client')))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../deployment/client/index.html'))
+// })
+
+app.listen(port, () => {
+    console.log('Docked at port ' + port)
+})
